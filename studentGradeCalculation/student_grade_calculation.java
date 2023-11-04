3e7213a82c3502dd19c19c61613ea199a49abe35
SUBRAJIT SAHOO <140285244+Subrajit2000@users.noreply.github.com>
2023-11-04 16:57:24 +0530

    Add files via upload

---
+++ studentGradeCalculation/student_grade_calculation.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        double totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            double marks = scanner.nextDouble();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = totalMarks / numSubjects;
+        char grade = calculateGrade(averagePercentage);
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+    }
+
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else if (averagePercentage >= 50) {
+            return 'E';
+        } else {
+            return 'F';
+        }
+    }
+}
